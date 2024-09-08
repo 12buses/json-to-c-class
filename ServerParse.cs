dbeb626a89a87c5f1cb1f5754cf30eaa53602205
12buses <155476004+12buses@users.noreply.github.com>
2024-09-09 00:04:15 +0300

    Переписал ваш скрипт, добавив десериализацию
    Должно работать, до этого пытался сделать это в .Net приложении, поэтому не мог использовать networking. Однако проверить не получиться, сайт с которого я хотел брать json файл для примера не хочет его отдавать при таком запросе, но опять же должно работать думаю. Также тут используется JSON framework for .NET, конечно можно было попробовать найти похожий пакет под юнити, но раз я уже умею работать с этим и возможность установить его есть, хоть и кривая почему бы и нет(https://github.com/GlitchEnzo/NuGetForUnity?tab=readme-ov-file)

---
+++ ServerParse.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+public class ServerParse : MonoBehaviour
+{
+    private string url = "https://www.banki.ru/products/currencyNodejsApi/getTrackingUrlList/"; // Замените на ваш URL
+
+    public class products
+    {
+        public int productId { get; set; }
+        public string category { get; set; }
+    }
+
+
+    public void Action()
+    {
+        // Запускаем корутин, который будет делать запрос
+        StartCoroutine(GetJsonData());
+    }
+
+    IEnumerator GetJsonData()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            // Отправляем запрос
+            yield return webRequest.SendWebRequest();
+
+            // Проверяем наличие ошибок
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Ошибка: " + webRequest.error);
+            }
+            else
+            {
+                // Получаем данные
+                string jsonResult = webRequest.downloadHandler.text;
+                Debug.Log("Полученные данные: " + jsonResult);
+
+                // Здесь вы можете обработать JSON данные
+                ProcessJsonData(jsonResult);
+            }
+        }
+    }
+
+    void ProcessJsonData(string json)
+    {
+        List <products> items = JsonConvert.DeserializeObject<List<products>>(json);
+        foreach (products cur in items)
+        {
+            Debug.Log(cur.productId + " " + cur.category);
+        }
+    }
+
+        // Здесь добавьте код для обработки первых полученных данных
+        // Например, десериализация JSON в объект
+}
